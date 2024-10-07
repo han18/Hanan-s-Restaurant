500098c3689fc2485dd4dc71489ab93e305625e2
han18 <han.fadah@gmail.com>
2024-10-07 11:41:49 -0400

    working within the DOM

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 // this is where all the functionality of the dom will go
-// testing the branching process
-// trying to understand the merging and branching precess
+// testing the branching process understand the merging and branching precess
+
+const header3 = document.querySelector(".header-3")
+header3.textContent = "I changed the header three text";
+
+
+
+
