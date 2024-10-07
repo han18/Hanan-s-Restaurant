41062fa8a8de77a7557d83a4b047c371947ee6df
han18 <han.fadah@gmail.com>
2024-10-07 12:35:21 -0400

    working on the darkmode through the DOM

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 // this is where all the functionality of the dom will go
 // testing the branching process understand the merging and branching precess
+// using LocalStorage Local Storage is designed to hold data indefinitely
 
 const header3 = document.querySelector(".header-3")
-header3.textContent = "I changed the header three text";
+header3.innerHTML = "<h3>I changed the header's text </h3>";
 
+//========================================//
+
+// this code is from online example
 
 
 
