255e1c6402315a24187675c79cbfcb1871f2c038
han18 <han.fadah@gmail.com>
2024-10-31 10:58:16 -0400

    creating the api function

--- api.js
+++ api.js
@@ -2,3 +2,17 @@
 
 // alert("this is an alert")
 
+// triggering the dom
+
+// first get the button and from the button get the value of the input to get the results 
+
+const searchButton = document.querySelector("searchButton");
+
+
+// because we are clicking the button triggring an event and calling a function
+searchButton.addEventListener("click", getRecipe);
+
+function getRecipe() {
+    // getting the search input
+    
+}
