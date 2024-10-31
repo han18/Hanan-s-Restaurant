5e6ef9502a0abbda8f8a31e65103017f41d2f12d
han18 <han.fadah@gmail.com>
2024-10-31 12:51:29 -0400

    setting the fetch function

--- api.js
+++ api.js
@@ -6,10 +6,11 @@
 
 // first get the button and from the button get the value of the input to get the results 
 
+
 const searchButton = document.querySelector(".searchButton");
 
 // url of main filltered ingredients  
-// const BASE_API = "www.themealdb.com/api/json/v1/1/filter.php?c=Seafood";
+const BASE_API = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 
 // because we are clicking the button triggring an event and calling a function
@@ -20,9 +21,32 @@ function getRecipe() {
     let searchInput = document.getElementById("searchInput").value.trim();
     console.log(searchInput);
     // using fetch instead of axios to fetch data
-    // fetch("www.themealdb.com/api/json/v1/1/filter.php?c=Seafood").then(response => response.json()).then(data => {
-    //     console.log(data);
-    // })
+    // checking if input is not empty
+    if (searchInput) {
+        fetch(`${BASE_API}${searchInput}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // getting the meals from data -- meal is a variable to hold the new search
+                data.meals.forEach(meal => {
+                    html += `
+                    <div> 
+                    <h1> ${meal.strMealThums} </h1>
+                    </div>
+                    <div>
+                    <img src="${meal.strmealThumb}">
+                    </div>
+                    
+                    `
+
+                })
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+            });
+    } else {
+        console.log("Please enter a recipe search.");
+    }
 
 
 }
